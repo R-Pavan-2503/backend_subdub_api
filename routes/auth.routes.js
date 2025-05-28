bc10b9239bef1250ad59e00696d7e0de40b9d949
Pavan <sara.pavansriram@gmail.com>
2025-05-28 21:43:31 +0530

    Controller has been added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,23 +1,11 @@
-import { Router } from "express";
+import { Router } from 'express';
+
+import { signUp, signIn, signOut } from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
-authRouter.post('/sign-up', (req, res) => res.send(
-    {
-        title: 'Sign UP'
-    }
-))
+authRouter.post('/sign-up', signUp);
+authRouter.post('/sign-in', signIn);
+authRouter.post('/sign-out', signOut);
 
-authRouter.post('/sign-in', (req, res) => res.send(
-    {
-        title: 'Sign In'
-    }
-))
-
-authRouter.post('/sign-out', (req, res) => res.send(
-    {
-        title: 'Sign Out'
-    }
-))
-
-export default authRouter
+export default authRouter;
