d67bdea1693f54f7ecc147e97795eb8ce7c0e9c7
Pavan <sara.pavansriram@gmail.com>
2025-06-02 14:57:38 +0530

    Subscription create has been added

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import { createSubscription } from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
@@ -16,12 +18,7 @@ subscriptionRouter.get('/:id', (req, res) => res.send(
     }
 ))
 
-subscriptionRouter.post('/', (req, res) => res.send(
-
-    {
-        title: 'Create subscription'
-    }
-))
+subscriptionRouter.post('/', authorize, createSubscription)
 
 subscriptionRouter.put('/:id', (req, res) => res.send(
 
